69b5af53900d0123daf18479b3da4bad2c4f716a
Gayashan Amarasinghe <gayashan@wso2.com>
2014-08-19 18:37:41 +0530

    Added script to run mvn clean for each directory which it is run

---
+++ mvn-clean-for-each-dir.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This script can be used to run mvn clean for each repository in the directory.
+# Useful when you want to clean multiple repositories.
+# How to use:
+#       cd <parent directory where all the repos reside>
+#       sh <path to the script>/mvn-clean-for-each-dir.sh
+#
+
+for D in *; do
+    if [ -d "${D}" ]; then
+        echo
+        echo "##########################################################################"
+        echo "${D}"
+        cd ${D}
+        mvn clean -o
+        cd ../
+    fi
+done
+
+echo
+echo "##########################################################################"
+echo "Done cleaning."
