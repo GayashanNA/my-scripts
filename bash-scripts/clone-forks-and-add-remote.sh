6f14deb4f046cf27a93143f34cdee2528a37472c
Gayashan Amarasinghe <gayashan@wso2.com>
2014-08-20 16:30:26 +0530

    Made the GitHub forks clonning script generic, can pass the username and org name as cmd line args.

--- bash-scripts/clone-forks-and-add-remote.sh
+++ bash-scripts/clone-forks-and-add-remote.sh
@@ -5,16 +5,21 @@
 # Useful when you want to clone multiple repos and add remotes to those.
 #
 # How to use:
-#   ./<path to the script>/clone-forks-and-add-remote.sh <file with list of forked repos>
+#   ./<path to the script>/clone-forks-and-add-remote.sh <file with list of forked repos> <GitHub user name> <GitHub organization name>
 #
-# Note: * THIS IS A WSO2 SPECIFIC SCRIPT!
-#       * The list of repos in the file should be each in a new line and the end of file
-#         should also contain a new line. Otherwise the last line will be omitted.
+# Note: The list of repos in the file should be each in a new line and the end of file
+#       should also contain a new line. Otherwise the last line will be omitted.
 #
 
+#TODO: can extract the list of forks from the GitHub API.
+
 FILE=$1
-USER_GIT_URL_PREFIX="https://github.com/GayashanNA/"
-UPSTREAM_GIT_URL_PREFIX="git@github.com:wso2-dev/"
+#Read GitHub user name from the input, if not given use my user name as default
+USER_NAME=${2:-"GayashanNA"}
+#Read GitHub organization name from the input, if not given use wso2-dev as default
+ORG_NAME=${3:-"wso2-dev"}
+USER_GIT_URL_PREFIX="https://github.com/${USER_NAME}/"
+UPSTREAM_GIT_URL_PREFIX="git@github.com:${ORG_NAME}/"
 GIT_SUFFIX=".git"
 
 #repos=( "carbon-utils" "carbon-deployment" )
@@ -24,16 +29,16 @@ while read -r line; do
     repo=$line
     my_git_repo=${USER_GIT_URL_PREFIX}${repo}${GIT_SUFFIX}
     upstream_git_repo=${UPSTREAM_GIT_URL_PREFIX}${repo}${GIT_SUFFIX}
-    upstream="wso2dev-${repo}"
+    upstream="${ORG_NAME}-${repo}"
 
     echo
     echo "Clonning : ${my_git_repo}"
-    git clone ${my_git_repo}
+    #git clone ${my_git_repo}
 
     echo "Adding remote ${upstream} to: ${upstream_git_repo}"
     cd ${repo}
-    git remote add ${upstream} ${upstream_git_repo}
-    git remote -v
+    #git remote add ${upstream} ${upstream_git_repo}
+    #git remote -v
     cd ../
     echo "############################ Done ########################################"
 done < $FILE
